c4cd48155c475e0b2b310cefa3e125abf254d275
Yavuz <yavuz.orbey@gmail.com>
2021-08-31 16:14:59 -0400

    4:14 PM changes

---
+++ src/BookYavuz.tsx
@@ -0,0 +1,13 @@
+
+
+
+const Book = (props) => {
+    return <div>
+        <h1>{props.title}</h1>
+        <span>{props.author}</span>
+        <span>{props.author}</span>
+        <span>{props.author}</span>
+        <span>{props.author}</span>
+
+    </div>
+}
